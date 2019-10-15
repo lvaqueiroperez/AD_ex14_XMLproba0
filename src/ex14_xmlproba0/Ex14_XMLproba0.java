374024ca99742651135c8f0a161f44d22ab4215f
oracle <oracle@localhost>
2019-10-15 10:37:09 +0200

    ex14 acabar

--- src/ex14_xmlproba0/Ex14_XMLproba0.java
+++ src/ex14_xmlproba0/Ex14_XMLproba0.java
@@ -11,16 +11,42 @@ public class Ex14_XMLproba0 {
 
     public static void main(String[] args) throws IOException, XMLStreamException {
 
-        File fich1 = new File("/home/oracle/Desktop/ex14");
+        File fich1 = new File("/home/oracle/Desktop/ex14/xmlproba0.xml");
         FileWriter fich1FW = new FileWriter(fich1);
-        
+
         XMLOutputFactory xmlOF1 = XMLOutputFactory.newInstance();
-        XMLStreamWriter xmlST1 = xmlOF1.createXMLStreamWriter(fich1FW);
-        
-        
-        
-        
-        
+        XMLStreamWriter xmlSW1 = xmlOF1.createXMLStreamWriter(fich1FW);
+
+        xmlSW1.writeStartDocument("1.0");
+
+        xmlSW1.writeStartElement("autores");
+        xmlSW1.writeStartElement("autor");
+        xmlSW1.writeAttribute("codigo", "a1");
+        xmlSW1.writeStartElement("nome");
+        xmlSW1.writeCharacters("Alexandre Dumas");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeStartElement("titulo");
+        xmlSW1.writeCharacters("El conde del monasterio");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeStartElement("titulo");
+        xmlSW1.writeCharacters("Los miserables");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeEndElement();
+        xmlSW1.writeStartElement("autor");
+        xmlSW1.writeAttribute("codigo", "a2");
+        xmlSW1.writeStartElement("nome");
+        xmlSW1.writeCharacters("Fiodor");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeStartElement("titulo");
+        xmlSW1.writeCharacters("El idiota");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeStartElement("titulo");
+        xmlSW1.writeCharacters("Noches Blancas");
+        xmlSW1.writeEndElement();
+        xmlSW1.writeEndElement();
+        xmlSW1.writeEndElement();
+        xmlSW1.writeEndDocument();
+
     }
 
 }
